a10664499841769e9bbebe0569fdd509a429b5b1
TanayJyot <tjsinghchawla@gmail.com>
2025-07-17 18:35:26 +0530

    ## Switching to SDK 53 for Expo
    This branch was an attempt to switch to SDK 53.
    
    However, it did not work because `native-base` has been deprecated and contains type casting issues for outlineWidth etc.

--- HokkienTranslation/backend/database/Firebase.js
+++ HokkienTranslation/backend/database/Firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, initializeAuth } from "firebase/auth";
 
 // Firebase Configuration
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
+const auth = initializeAuth(app);
 
 export { db, auth };
 export default app;
